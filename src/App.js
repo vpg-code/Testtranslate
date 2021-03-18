7c63e38fae54296f27deea971f9f8e5dc8c377a6
Mackan <vpgstudiodev@gmail.com>
2021-03-18 13:03:21 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Translator, Translate} from 'react-auto-translate';
+
+
+export default App() {
+  // example procider
+  const CacheProvider = {
+  get: (language, key) =>
+    ((JSON.parse(localStorage.getItem('translations')) || {})[key] || {})[
+      language
+    ],
+  set: (language, key, value) => {
+    const existing = JSON.parse(localStorage.getItem('translations')) || {
+      [key]: {},
+    };
+    existing[key] = {...existing[key], [language]: value};
+    localStorage.setItem('translations', JSON.stringify(existing));
+  },
+  };
+
+  return (
+    <Translator
+        cacheProvider={CacheProvider}
+        from='en'
+        to='es'
+        googleApiKey='AIzaSyDb2ZLcdT5NCF9IdVeOKLhbYJk8PNv3udE'
+      >
+      
+      <h1><Translate>Welcome!</Translate></h1>
+    </Translator>
+  );
+}
